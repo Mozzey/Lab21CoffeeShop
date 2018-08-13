68b28e1e289ec0a7e7a4735fb05e13e76cc080ed
Mozzey <mbcacciano@gmail.com>
2018-08-13 16:11:56 -0400

    still need to tweak the ui

---
+++ Lab21CoffeeShop/Lab21CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Lab21CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Welcome(UserModel user)
+        {
+            ViewBag.Message = user.FullName;
+            return View(user);
+        }
+    }
+}
---
+++ Lab21CoffeeShop/Lab21CoffeeShop/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab21CoffeeShop.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please enter your first name")]
+        [StringLength(15, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(15, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please a valid email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
